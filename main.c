d9a92d89148c2e9fd58d0f916a4d1b9144635c36
nick-ras <nickras@mail.dk>
2022-07-14 15:36:56 +0200

    making commands

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ t_list	*ft_lstnew_new(char *content)
 	return (new);
 }
 
-t_list	*ft_lstnew(int zero)
+t_list	*ft_lstnew_empty(int zero)
 {
 	t_list	*new;
 
@@ -38,39 +38,21 @@ t_list	*ft_lstnew(int zero)
 	return (new);
 }
 
-t_list *create_linked_list(int argc, char **argv)
+t_list	*ft_lstlast(t_list *lst)
 {
-	int i;
-	t_list *ptr_first;
-	t_list *stack_a;
-	t_list	*ox2;
+	int		i;
+	t_list	*last_a;
 
-	if (argc <= 1)
-		return (0);
-	printf("argc: %d\n", argc);
-	i = 1;
-	stack_a = ft_lstnew_new(argv[i]);
-	stack_a->previous = NULL;
-	i++;
-	ptr_first = stack_a;
-	printf("ox2: %d and stirng: %s -- now loop->\n", ptr_first->num, argv[1]);
-	while (i < argc)
+	if (!lst)
+		return (NULL);
+	last_a = lst;
+	i = 0;
+	while (last_a->next)
 	{
-		stack_a->next = ft_lstnew_new(argv[i]);
-		ox2 = stack_a;
-		stack_a = stack_a->next;
-		stack_a->previous = ox2;
-		printf("in loop: index: %d contains: %d\n", i, stack_a->num);
 		i++;
+		last_a = last_a->next;
 	}
-	stack_a->next = NULL;
-	while(i > 1)
-	{
-		printf("in loop: index: %d contains: %d\n", i, stack_a->num);
-		stack_a = stack_a->previous;
-		i--;
-	}
-	return(ptr_first);
+	return (last_a);
 }
 
 t_list *create_linked_list(int argc, char **argv)
@@ -79,7 +61,6 @@ t_list *create_linked_list(int argc, char **argv)
 	t_list *ptr_first;
 	t_list *stack_a;
 	t_list	*ox2;
-	int	zero = 0;
 
 	if (argc <= 1)
 		return (0);
@@ -89,24 +70,57 @@ t_list *create_linked_list(int argc, char **argv)
 	stack_a->previous = NULL;
 	i++;
 	ptr_first = stack_a;
-	printf("ox2: %d and stirng: %s -- now loop->\n", ptr_first->num, argv[1]);
+	//printf("ox2: %d and stirng: %s -- now loop->\n", ptr_first->num, argv[1]);
 	while (i < argc)
 	{
-		stack_a->next = ft_lstnew(int zero);
-		//HOW IS LST NEW??
+		stack_a->next = ft_lstnew_new(argv[i]);
 		ox2 = stack_a;
 		stack_a = stack_a->next;
 		stack_a->previous = ox2;
-		printf("in loop: index: %d contains: %d\n", i, stack_a->num);
+		//printf("in loop: index: %d contains: %d\n", i, stack_a->num);
 		i++;
 	}
 	stack_a->next = NULL;
 	while(i > 1)
 	{
-		printf("in loop: index: %d contains: %d\n", i, stack_a->num);
+		//printf("in loop: index: %d contains: %d\n", i, stack_a->num);
 		stack_a = stack_a->previous;
 		i--;
 	}
+	return(ptr_first);
+}
+
+t_list *create_empty_linked_list(int argc, int zero)
+{
+	int i;
+	t_list *ptr_first;
+	t_list *stack_a;
+	t_list	*ox2;
+	if (argc <= 1)
+		return (0);
+	printf("argc: %d\n", argc);
+	i = 1;
+	stack_a = ft_lstnew_empty(zero);
+	stack_a->previous = NULL;
+	i++;
+	ptr_first = stack_a;
+	while (i < argc)
+	{
+		stack_a->next = ft_lstnew_empty(zero);
+		//HOW IS LST NEW??
+		ox2 = stack_a;
+		stack_a = stack_a->next;
+		stack_a->previous = ox2;
+		//printf("in loop: index: %d contains: %d\n", i, stack_a->num);
+		i++;
+	}
+	stack_a->next = NULL;
+	/* while(i > 1)
+	{
+		printf("in loop: index: %d contains: %d\n", i, stack_a->num);
+		stack_a = stack_a->previous;
+		i--;
+	} */
 	return(ptr_first);
 }
 
@@ -128,12 +142,110 @@ void sa(t_list *ox2)
 	ox1->next = NULL;
 }
 
+void	pa(t_list *stack_a, t_list *stack_b)
+{
+	t_list	*temp_pa;
+	t_list	*last_a;
+	t_list	*last_b;
+
+	last_a = ft_lstlast(stack_a);
+	last_b = ft_lstlast(stack_b);
+	temp_pa = last_b->previous;
+	temp_pa->next = NULL;
+	last_a->next = last_b;
+	temp_pa= last_a->next;
+	//write(1, "testtesttes\n", 12);
+	temp_pa->next = NULL;
+}
+
+void	pb(t_list *stack_a, t_list *stack_b)
+{
+	t_list	*temp_pb;
+	t_list	*last_a;
+	t_list	*last_b;
+
+	last_a = ft_lstlast(stack_a);
+	last_b = ft_lstlast(stack_b);
+	temp_pb = last_a->previous;
+	temp_pb->next = NULL;
+	last_b->next = last_a;
+	temp_pb= last_b->next;
+	//write(1, "testtesttes\n", 12);
+	temp_pb->next = NULL;
+}
+
+void	ra(t_list *stack_a)
+{
+	t_list	*temp_ra;
+	t_list	*last_a;
+
+	while (stack_a->previous)
+		stack_a= stack_a->previous;
+	last_a = ft_lstlast(stack_a);
+
+	temp_ra = stack_a->next;
+	temp_ra->previous = NULL;
+	last_a->next = stack_a;
+	stack_a->next = NULL;
+}
+
+void	rra(t_list *stack_a)
+{
+	t_list	*temp_ra;
+	t_list	*last_a;
+
+	while (stack_a->previous)
+		stack_a= stack_a->previous;
+	last_a = ft_lstlast(stack_a);
+	temp_ra = last_a->previous;
+	temp_ra->next = NULL;
+	stack_a->previous = last_a;
+	last_a->next = stack_a;
+	last_a->previous = NULL;
+}
+
+void	rrb(t_list *stack_b)
+{
+	t_list	*temp_rb;
+	t_list	*last_b;
+
+	while (stack_b->previous)
+		stack_b= stack_b->previous;
+	last_b = ft_lstlast(stack_b);
+	temp_rb = last_b->previous;
+	temp_rb->next = NULL;
+	stack_b->previous = last_b;
+	last_b->next = stack_b;
+	last_b->previous = NULL;
+}
+
 void	commands(t_list *stack_a, t_list *stack_b, int command)
 {
 	if (command == 0)
 		sa(stack_a);
 	if (command == 1)
 		sa(stack_b);
+	if (command == 2)
+	{
+		sa(stack_a);
+		sa(stack_b);
+	}
+	if (command == 3)
+		pa(stack_a, stack_b);
+	if (command == 4)
+		pb(stack_a, stack_b);
+	if (command == 5)
+		ra(stack_a);
+	/* if (command == 6)
+		rb(stack_a, stack_b);
+	if (command == 7)
+		rr(stack_a, stack_b);*/
+	if (command == 8)
+		rra(stack_a);
+	if (command == 9)
+		rrb(stack_b);
+	/* if (command == 10)
+		rrr(stack_b); */
 }
 
 int main(int argc, char **argv)
@@ -142,24 +254,25 @@ int main(int argc, char **argv)
 	t_list	*stack_b;
 	int	command;
 
-	command = 1;
 	if(argc > 1000)
 	{
 		write(1, "argc too big", 12);
 		return(1);
 	}
 	stack_a = create_linked_list(argc, argv);
-	stack_b = create_empty_linked_list(argc, argv);
-	//HOWTO make empty array efficient?
+	stack_b = create_empty_linked_list(argc, 0);
+	command = 5;
 	commands(stack_a, stack_b, command);
-	int i = 0;
-	//ptr_first = ptr_first->previous;
+	//stack_a= stack_a->next;
+	//stack_b= stack_b->previous;
 	while(stack_a)
 	{
-		printf("--------------ptr = %d\n", stack_a->num);
-		stack_a = stack_a->next;
-		i++;
+		printf("--------------main test stack a = %d\n", stack_a->num);
+		stack_a = stack_a->previous;
 	}
-	
-	
+	while(stack_b)
+	{
+		printf("--------------stack b =           %d\n", stack_b->num);
+		stack_b = stack_b->next;
+	}
 }
