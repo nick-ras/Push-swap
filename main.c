e3adae167875efa848ccc03cd616fc8f066c8000
nick-ras <nickras@mail.dk>
2022-07-15 13:46:59 +0200

    tring to sort numbers in linked lists

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ t_list	*ft_lstnew_new(char *content)
 		write(1, "atoi returned error\n", 19);
 		exit(1);
 	}
-	printf("Atoi: %d\n", new->num);
+	//printf("Atoi: %d\n", new->num);
 	return (new);
 }
 
@@ -90,40 +90,6 @@ t_list *create_linked_list(int argc, char **argv)
 	return(ptr_first);
 }
 
-t_list *create_empty_linked_list(int argc, int zero)
-{
-	int i;
-	t_list *ptr_first;
-	t_list *stack_a;
-	t_list	*ox2;
-	if (argc <= 1)
-		return (0);
-	printf("argc: %d\n", argc);
-	i = 1;
-	stack_a = ft_lstnew_empty(zero);
-	stack_a->previous = NULL;
-	i++;
-	ptr_first = stack_a;
-	while (i < argc)
-	{
-		stack_a->next = ft_lstnew_empty(zero);
-		//HOW IS LST NEW??
-		ox2 = stack_a;
-		stack_a = stack_a->next;
-		stack_a->previous = ox2;
-		//printf("in loop: index: %d contains: %d\n", i, stack_a->num);
-		i++;
-	}
-	stack_a->next = NULL;
-	/* while(i > 1)
-	{
-		printf("in loop: index: %d contains: %d\n", i, stack_a->num);
-		stack_a = stack_a->previous;
-		i--;
-	} */
-	return(ptr_first);
-}
-
 void sa(t_list *ox2)
 {
 	t_list *ox1;
@@ -170,8 +136,8 @@ void	pb(t_list *stack_a, t_list *stack_b)
 	temp_pb->next = NULL;
 	last_b->next = last_a;
 	temp_pb= last_b->next;
-	//write(1, "testtesttes\n", 12);
 	temp_pb->next = NULL;
+	printf("++pb functino next   ----> %p\n", stack_b->next);
 }
 
 void	ra(t_list *stack_a)
@@ -198,7 +164,6 @@ void	rb(t_list *stack_b)
 	while (stack_b->previous)
 		stack_b= stack_b->previous;
 	last_b = ft_lstlast(stack_b);
-
 	temp_rb = stack_b->next;
 	last_b->next = stack_b;
 	stack_b->previous = last_b;
@@ -271,11 +236,35 @@ void	commands(t_list *stack_a, t_list *stack_b, int command)
 	}
 }
 
+int	sort_check(t_list *stack_a)
+{
+	t_list	*next_nb;
+
+	next_nb = stack_a;
+	while (next_nb->previous)
+		next_nb= next_nb->previous;
+	stack_a = next_nb;
+	while (stack_a->next)
+	{
+		next_nb = stack_a->next;
+		if(stack_a->num > next_nb->num)
+			return (1);
+		stack_a = stack_a->next;
+	}
+	return (0);
+}
+
+
 int main(int argc, char **argv)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
+	t_list	*temp_a;
+	t_list	*temp_b;
+	t_list	*temp_a_nested;
+	t_list	*temp_b_nested;
 	int	command;
+	int	exp = 2;
 
 	if(argc > 1000)
 	{
@@ -283,23 +272,51 @@ int main(int argc, char **argv)
 		return(1);
 	}
 	stack_a = create_linked_list(argc, argv);
-	stack_b = create_empty_linked_list(argc, 0);
+	stack_b = ft_lstnew_empty(0);
+	stack_b->previous = NULL;
+	temp_a = ft_lstlast(stack_a);
+	printf("temp_a ----> %d\n", temp_a->num);
+	temp_b = stack_b;
+	// temp_b = temp_b;
+	// temp_a_nested = stack_a;
+	// temp_b_nested = stack_b;
 	command = 7;
+	if (argc <=5)
+	{
+		write(1, "small\n", 6);
+	}
+	else
+	{
+		while (exp < 1073741824 )
+		{
+			//CHECK IF SORTED
+			//push to b
+			while(temp_a->previous)
+			{
+				printf("--------------------------------\n");
+				temp_a_nested = temp_a;
+				temp_a = temp_a->previous;
+				if (temp_a_nested->num%exp == 1)
+				{
+					printf("temp_a prev----> %d\n", temp_a->num);
+					commands(temp_a_nested, temp_b, 4);
+					printf("temp_b next---> %p\n", temp_b->next);
+					temp_b = temp_b->next;
+					printf("temp_b in loop ----> %d\n", temp_b->num);
+					printf("temp_b prev---> %p\n", temp_b->previous);
+				}
+				else
+					commands(temp_a, stack_b, 8);
+			}
+			//push to a
+			while(temp_b_nested->previous)
+			{
+				commands(temp_a_nested, temp_b, 3);
+				temp_b_nested = temp_b_nested->previous;
+			}
+			temp_a = temp_a->previous;
+		}
+		exp = exp * 2;
+	}
 	commands(stack_a, stack_b, command);
-	//stack_a= stack_a->next;
-	//stack_b= stack_b->previous;
-	while (stack_a->previous)
-		stack_a = stack_a->previous;
-	while (stack_b->previous)
-		stack_b = stack_b->previous;
-	while(stack_a)
-	{
-		printf("--------------main test stack a = %d\n", stack_a->num);
-		stack_a = stack_a->next;
-	}
-	while(stack_b)
-	{
-		printf("--------------stack b =           %d\n", stack_b->num);
-		stack_b = stack_b->next;
-	}
 }
