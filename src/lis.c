01ef48f49bc56d575e94ba7d75b25d8698e7df2d
nick rasmussen <nick-ras@mail.dk>
2022-08-14 18:57:37 +0200

    small stuff

--- src/lis.c
+++ src/lis.c
@@ -17,17 +17,19 @@ void	make_lis(t_push *stack_a)
 				if (stack_a->num < outer->num)
 				{
 					if (stack_a->len + 1 >= outer->len)
-						{
-							outer->len = stack_a->len + 1;
-							outer->subs = stack_a;
-						}
+					{
+						outer->len = stack_a->len + 1;
+						outer->subs = stack_a;
+						if (!stack_a->subs)
+							stack_a->subs = stack_a;
+					}
 				}
 			}
 			stack_a = stack_a->next;
 		}
 		outer = outer->next;
 	}
-	clean_lis(stack_a);
+	clean_lis(first);
 }
 
 void	clean_lis(t_push *stack_a)
@@ -45,15 +47,12 @@ void	clean_lis(t_push *stack_a)
 		}
 		stack_a = stack_a->next;
 	}
-	stack_a = tmp;
+	stack_a = tmp->prev;
+	tmp = tmp->subs;
 	while (stack_a)
 	{
-		if (stack_a->subs)
-		{
+		if (stack_a == tmp)
 			tmp = tmp->subs;
-			if (!tmp->subs)
-				tmp->subs = tmp;
-		}
 		else
 			stack_a->subs = NULL;
 		stack_a = stack_a->prev;
